72b6d1d959dfd38d9252e4000d27e9e49f894618
Bartosz Sułkowski <mail@bsulkowski.pl>
2018-04-14 00:18:39 +0200

    reduced redundancy in base shape definition

--- generate_piece_base.py
+++ generate_piece_base.py
@@ -5,6 +5,22 @@ version = "2.2"
 layer = 5
 spacing = layer * 6
 base_stroke = 0.3
+
+def merge(list, separator):
+    return reduce(lambda x, y: x + separator + y, list)
+
+def mirror(points):
+    return points + [(-x, y) for (x, y) in reversed(points)]
+
+fancy_edge = [
+    (4,  0),
+    (3.5, 1),
+    (2.5, 1.5),
+    (2, 2.5),
+    (2, 3.5),
+    (3, 5.5),
+    (2,  6)
+]
     
 with open(f'piece_base_{layer}mm_v{version}.svg', 'w') as f:
     f.write(f'''\
@@ -43,66 +59,32 @@ with open(f'piece_base_{layer}mm_v{version}.svg', 'w') as f:
         </g>
         <g id="1" transform="translate({12 + 6 + 2}, 0) rotate(90)">
             <polygon points="
-                 1.1,  2
-                 1.1,  0
-                 3,  0
-                 3, -1.9
-                 4, -1.9
-                 4,  0
-                 3.5, 1
-                 2.5, 1.5
-                 2, 2.5
-                 2, 3.5
-                 3, 5.5
-                 2,  6
-                 1.1,  6
-                 1.1,  3.9
-                -1.1,  3.9
-                -1.1,  6
-                -2,  6
-                -3, 5.5
-                -2, 3.5
-                -2, 2.5
-                -2.5, 1.5
-                -3.5, 1
-                -4, 0
-                -4, -1.9
-                -3, -1.9
-                -3,  0
-                -1.1,  0
-                -1.1,  2
+                 ''' + merge([f'{x}, {y}' for (x, y) in mirror([
+                 (1.1,  2),
+                 (1.1,  0),
+                 (3,  0),
+                 (3, -1.9),
+                 (4, -1.9)
+                 ] + fancy_edge + [
+                 (1.1,  6),
+                 (1.1,  3.9)
+                 ])], '''
+                 ''') + f'''
             "/>
         </g>
         <g id="2" transform="translate({24 + 6 + 8}, 0) rotate(90)">
             <polygon points="
-                 1.1,  4
-                 1.1,  0
-                 3,  0
-                 3, -1.9
-                 4, -1.9
-                 4,  0
-                 3.5, 1
-                 2.5, 1.5
-                 2, 2.5
-                 2, 3.5
-                 3, 5.5
-                 2,  6
-                 1,  6
-                 1, 14
-                -1, 14
-                -1,  6
-                -2,  6
-                -3, 5.5
-                -2, 3.5
-                -2, 2.5
-                -2.5, 1.5
-                -3.5, 1
-                -4, 0
-                -4, -1.9
-                -3, -1.9
-                -3,  0
-                -1.1,  0
-                -1.1,  4
+                 ''' + merge([f'{x}, {y}' for (x, y) in mirror([
+                 (1.1,  4),
+                 (1.1,  0),
+                 (3,  0),
+                 (3, -1.9),
+                 (4, -1.9)
+                 ] + fancy_edge + [
+                 (1,  6),
+                 (1, 14)
+                 ])], '''
+                 ''') + f'''
             "/>
         </g>
     </g>
